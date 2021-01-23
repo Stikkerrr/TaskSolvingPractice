a8b2e0c75a35184affec3f71163008b826f7779e
Stikkerrr <storento@yandex.ru>
2021-01-23 22:41:32 +0300

    4. Вывести на консоль числа от M до N.

---
+++ src/job4j/loop/BackwardFor.java
@@ -0,0 +1,15 @@
+package job4j.loop;
+
+public class BackwardFor {
+	
+	public static void out(int m, int n) {
+		for (int i = m; i > n; i--) {
+			System.out.println(i);
+		}
+	}
+}
+
+/* 4. Вывести на консоль числа от M до N.
+ *
+ * Нужно вывести числа от M до N. N не включено. M > N.
+ */
