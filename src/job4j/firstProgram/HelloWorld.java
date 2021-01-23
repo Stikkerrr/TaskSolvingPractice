a3bdd93e0d25ab0d1959ae233333b5c3f4fac5d7
Stikkerrr <storento@yandex.ru>
2021-01-23 03:16:57 +0300

    1. Забытая точка с запятой

---
+++ src/job4j/firstProgram/HelloWorld.java
@@ -0,0 +1,11 @@
+package job4j.firstProgram;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, Job4j!");
+	}
+}
+
+/*
+ * Каждый оператор в Java должен заканчиваться точкой с запятой. Исправьте ошибку в приведенном коде.
+ */
