04a21c9383dab886c11ee4c6a985723179230fd8
Stikkerrr <storento@yandex.ru>
2021-01-27 11:38:33 +0300

    19. Вывести нечетные в порядке возрастания индексов

--- src/job4j/array/SummingPrevious.java
+++ src/job4j/array/SummingPrevious.java
@@ -1,13 +1,14 @@
 package job4j.array;
 
 public class SummingPrevious {
+	
 	public static int[] calculate(int a, int b, int n) {
 		int[] rsl = new int[n];
 		rsl[0] = a;
 		rsl[1] = b;
 		int sum = a;
 		for (int i = 2; i < n; i++) {
-			sum += rsl[i-1];
+			sum += rsl[i - 1];
 			rsl[i] = sum;
 		}
 		return rsl;
