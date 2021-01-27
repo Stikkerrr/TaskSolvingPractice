ff3c7da45cc4f2f96ae0881d2c19bf2aa7c0bfe1
Stikkerrr <storento@yandex.ru>
2021-01-27 11:23:46 +0300

    17. Транспонировать матрицу

--- src/job4j/array/TriangleMatrix.java
+++ src/job4j/array/TriangleMatrix.java
@@ -1,14 +1,12 @@
 package job4j.array;
 
-import java.util.Arrays;
-
 public class TriangleMatrix {
 	
 	public static int[][] rows(int count) {
 		int[][] triangle = new int[count][];
-		int counter = 1;
+		int     counter  = 1;
 		for (int i = 0; i < triangle.length; i++) {
-			triangle[i] = new int[i+1];
+			triangle[i] = new int[i + 1];
 			for (int j = 0; j <= i; j++) {
 				triangle[i][j] = counter++;
 			}
