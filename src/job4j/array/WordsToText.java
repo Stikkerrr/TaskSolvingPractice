3d5dabc9d44a6ca69fb9334e87291c408d2a8ae4
Stikkerrr <storento@yandex.ru>
2021-01-29 16:33:42 +0300

    42. Объединение слов

---
+++ src/job4j/array/WordsToText.java
@@ -0,0 +1,16 @@
+package job4j.array;
+
+public class WordsToText {
+	public static String convert(String[] words) {
+		return String.join(" ", words);
+	}
+}
+
+/* 42. Объединение слов
+ *
+ * Дан массив слов, необходимо получить текст из этих слов, разделенных пробелом.
+ *
+ * Например,
+ *
+ * {"one", "two", "three"} => "one two three"
+ */
